b7ad68dde1f16525ecf2867bcc693e199ee1a077
Hugo Cortes <hcortes@mydevices.com>
2020-03-24 17:10:43 -0700

    feat(core): class to instantiate all services

--- test/service.test.js
+++ test/service.test.js
@@ -13,6 +13,36 @@ const {
 } = require('./env');
 
 describe('[Service]', () => {
+  it('Should be able to issue authenticated requests from Core', async () => {
+    const serviceOpts = {
+      cayenne: cayenneOpts,
+      deloreanES: deloreanESOpts,
+      executor: executorOpts,
+      hermes: hermesOpts,
+      prometheus: prometheusOpts,
+      timekeeper: timekeeperOpts
+    };
+    const opts = { logger: console.log };
+    const core = new Core.Core(serviceOpts, opts);
+    await Promise.all(
+      Object.entries(core).map(async ([name, clazz]) => {
+        console.log(`service under test: ${name}`);
+        await Promise.all(
+          Object.getOwnPropertyNames(clazz.__proto__).map(async key => {
+            if (!key.startsWith('get')) {
+              return;
+            }
+            const { status } = await clazz[key]()
+              .then(() => ({ status: 200 }))
+              .catch(({ status }) => ({ status }));
+            expect(status).to.not.equal(401);
+            expect(status).to.not.equal(403);
+          })
+        );
+      })
+    );
+  });
+
   it('Should be able to issue authenticated requests', async () => {
     await Promise.all(
       [
